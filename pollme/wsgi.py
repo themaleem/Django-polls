914d6490bc0ce7e95988ff6c0b031f06c16069b4
themaleem <zizo9585@gmail.com>
2020-04-06 12:48:32 +0100

    .

--- pollme/wsgi.py
+++ pollme/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pollme.settings')
 
 application = get_wsgi_application()
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+application = WhiteNoise(application, root=os.path.join(BASE_DIR,'/static'))
